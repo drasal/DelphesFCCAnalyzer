8b576398313ac75a3faeb8c7756b9aa1da8b1bc1
Zbynek Drasal <Zbynek.Drasal@cern.ch>
2016-03-02 11:37:24 +0100

    Added setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export BOOST_LIB="/usr/lib/x86_64-linux-gnu/"
+export BOOST_INCLUDE="/usr/include"
+export PODIO=/home/drasal/work/fcc/FCC-PODIO/PODIO/install
+export FCCEDM=/home/drasal/work/fcc/FCC-PODIO/FCC-EDM/install
+export ROOT_SYS="/usr/bin"
+export ROOT_CONFIG=${ROOT_SYS} 
